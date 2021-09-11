185ffcc4ffcd3c960f7ef4133de3e5d0ac9674eb
Kersten Schaller <kersten.schaller@outlook.com>
2021-09-11 18:52:24 +0200

    solved issues and refactored

--- LinuxBackgroundInfoScreen/main.cpp
+++ LinuxBackgroundInfoScreen/main.cpp
@@ -13,95 +13,207 @@
 #include <thread>
 #include <chrono>
 
+#include <fstream>
+
 using namespace cimg_library;
 using namespace std;
 
-void createImage(string text, string path)
+bool fileExists(const std::string &name)
 {
-  // get screen resolution
-  Display *display = XOpenDisplay(NULL);
-  Screen *screen = DefaultScreenOfDisplay(display);
-
-  // Create 640x480 image
-  CImg<unsigned char> image(screen->width, screen->height, 1, 3);
-
-  // Fill with black
-  cimg_forXY(image, x, y)
-  {
-    image(x, y, 0, 0) = 0;
-    image(x, y, 0, 1) = 0;
-    image(x, y, 0, 2) = 0;
-  }
-
-  // Make some colours
-  unsigned char white[] = {255, 255, 255};
-  unsigned char black[] = {0, 0, 0};
-
-  // Draw text
-  image.draw_text(30, 150, text.c_str(), white, black, 1, 32);
-
-  // Save result image
-  image.save_bmp(path.c_str());
+  ifstream f(name.c_str());
+  return f.good();
 }
 
-// exec bash command and get return value as string
-std::string exec(const char *cmd)
+class InfoScreen
 {
-  std::array<char, 128> buffer;
-  std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-  if (!pipe)
-  {
-    throw std::runtime_error("popen() failed!");
-  }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-  {
-    result += buffer.data();
-  }
-  return result;
-}
 
-/*
+public:
+  inline InfoScreen(string argv0, string directory, bool debug)
+  {
+    workDir = argv0;
+    workDir = eraseSubStr(workDir, "/DesktopbackgroundInfoScreen");
+    filepath = directory + "/info.bmp";
+    debugFlag = debug;
+    // get screen resolution
+    Display *display = XOpenDisplay(NULL);
+    Screen *screen = DefaultScreenOfDisplay(display);
+    x_res = screen->width;
+    y_res = screen->height;
+    log("info.bmp will be written to path: " + filepath, true);
+    log("setWallpaper script will be searched in path: " + workDir, true);
+  }
+  inline int setStatusText()
+  {
+    statusText = UpdateNordVPN();
+    bool statechanged = setState(statusText);
+    if (statechanged || ( count >= 10 ))
+    {
+      createImage(statusText, filepath);
+      string cmdstring = workDir + "/setWallpaper " + filepath + " manual >/dev/null 2>&1";
+      exec(cmdstring.c_str());
+      count = 0;
+    }
+    count++;
+    return 2;
+  }
+
+  inline bool setState(string text)
+  {
+      bool stateChanged = false;
+      if(text.find("Disconnected")!= string::npos)
+      {
+        currentState = DISCONNECTED;
+      }
+      else
+      {
+        if(text.find("Connected") != string::npos)
+        {
+          currentState = CONNECTED;
+        }
+      }
+
+      if(lastState != currentState)
+      {
+        stateChanged = true;
+        log("State changed.", debugFlag);
+      }
+      else
+      {
+        stateChanged = false;
+      }
+      lastState = currentState;
+      return stateChanged;
+
+
+  }
+
+private:
+  string errortext = "";
+  string statusText = "";
+  string workDir;
+  string filepath;
+  bool debugFlag = false;
+  int count = 0;
+  typedef enum State{CONNECTED, DISCONNECTED} State_e;
+  State lastState = DISCONNECTED;
+  State currentState = DISCONNECTED;
+
+  int x_res;
+  int y_res;
+
+  inline void createImage(string text, string path)
+  {
+
+    // Create image
+    CImg<unsigned char> image(x_res, y_res, 1, 3);
+
+    // Fill with black
+    cimg_forXY(image, x, y)
+    {
+      image(x, y, 0, 0) = 0;
+      image(x, y, 0, 1) = 0;
+      image(x, y, 0, 2) = 0;
+    }
+
+    // Make some colours
+    unsigned char white[] = {255, 255, 255};
+    unsigned char black[] = {0, 0, 0};
+
+    // Draw text
+    image.draw_text(30, 150, text.c_str(), white, black, 1, 32);
+
+    // Save result image
+    image.save_bmp(path.c_str());
+  }
+
+  // exec bash command and get return value as string
+  inline std::string exec(const char *cmd)
+  {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe)
+    {
+      throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
+      result += buffer.data();
+    }
+    return result;
+  }
+
+  /*
  * Erase First Occurrence of given  substring from main string.
  */
-string eraseSubStr(std::string &mainStr, const std::string &toErase)
-{
-  // Search for the substring in string
-  size_t pos = mainStr.find(toErase);
-  if (pos != std::string::npos)
+  inline string eraseSubStr(std::string &mainStr, const std::string &toErase)
   {
-    // If found then erase it from string
-    mainStr.erase(pos, toErase.length());
+    // Search for the substring in string
+    size_t pos = mainStr.find(toErase);
+    if (pos != std::string::npos)
+    {
+      // If found then erase it from string
+      mainStr.erase(pos, toErase.length());
+    }
+    return mainStr;
   }
-  return mainStr;
-}
+
+  void notifyUser(string text)
+  {
+    string fileName = "/usr/bin/notify-send ";
+    string commandString = fileName + "\"" + text + "\"";
+    exec(commandString.c_str());
+  }
+
+  string UpdateNordVPN()
+  {
+    return exec("nordvpn status");
+  }
+
+  void log(string text, bool debugflag)
+  {
+    if (debugflag)
+    {
+      cout << string(text).c_str() << endl;
+    }
+  }
+};
 
 int main(int argc, char *argv[])
 {
+
+  string directory;
+  if (argc >= 2)
+  {
+    directory = argv[1];
+    if(directory.back() == '/')
+    {
+      directory.pop_back();
+    }
+  }
+  else
+  {
+    cout << "Error: No Filepath given" << endl;
+    cout << "Please specify path where a temp file can be saved." << endl;
+    return EXIT_FAILURE;
+  }
+  bool debugflag = ((argc == 3) && (string(argv[2]) == "d") && (fileExists("/usr/bin/notify-send")));
+  InfoScreen infoScreen(argv[0], directory, debugflag);
+
   while (true)
   {
-    string statusText = exec("nordvpn status");
-
-    string directory;
-    if (argc > 1)
+    try
     {
-      directory = argv[1];
+      int sleeptime = infoScreen.setStatusText();
+      this_thread::sleep_for(chrono::seconds(sleeptime));
     }
-    else
+    catch (const std::exception &e)
     {
-      cout << "Error: No Filepath given" << endl;
-      cout << "Please specify path where a temp file can be saved." << endl;
-      return EXIT_FAILURE;
+      if (debugflag)
+      {
+        cout << "error: " << e.what();
+      }
     }
-    string workDir = argv[0];
-    workDir = eraseSubStr(workDir, "/DesktopbackgroundInfoScreen");
-
-    string filepath = directory + "/info.bmp";
-    createImage(statusText, filepath);
-    string cmdstring = workDir + "/setWallpaper " + filepath + " manual >/dev/null 2>&1";
-    system(cmdstring.c_str());
-
-    this_thread::sleep_for(chrono::seconds(2));
   }
 
   return EXIT_SUCCESS;
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # DesktopInfoBackgroundForMoksha
-Small hacky application which sets the background to black and embeds info text about nordvpn connection
+Small hacky application which sets the background to black and embeds info text about nordvpn connection.
+It updates the screen every 20 seconds or if the vpn status changes.
+Tested on Bodhi Linux
+
+## Build
+cmake . -bbuild
+cd build
+make
+
+
+## Install:
+Copy the executable "DesktopbackgroundInfoScreen" and the script "setWallpaper" to some place of your choice.
+Write some startup script for you system calling the executable with full path like this
+<full path>/DesktopbackgroundInfoScreen <path temp file>
+where <path temp file> is a folder where a an image can be saved which is used as a wallpaper. 
+
+Install and enable module slideshow.
+
+
+
+## Depenencies:
+1. notify-send , expected path /usr/bin/notify-send
+installable with
+sudo apt install libnotify-bin
+
+2. On Bodhi Linux it works in conjunction with module slide show.
+   The programm itself updated the wallpaper sources while slideshow is responsible for updating the screen.
